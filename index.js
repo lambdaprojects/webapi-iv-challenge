1cd1228944466df5c05ea900562597ed0d268c44
G3Ram <gayathri.t.ramakrishnan@gmail.com>
2019-05-30 15:49:20 -0500

    Added updates to environment variables and package.json

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require("dotenv").config();
+
 const server = require("./server.js");
 
-const port = "8000";
+const port = process.env.PORT || 8000;
 
 server.listen(port, () => {
   console.log(`API is running and server is listening on port ${port}`);
